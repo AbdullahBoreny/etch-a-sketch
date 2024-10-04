a9010b2c6b4c9bcfce773d6a411c2b93bb7f2daa
AbdullahBoreny <abdullah55boreny@gmail.com>
2024-10-04 06:22:58 +0300

    add an trail and create gird functions

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
-const cdiv = document.querySelector('.container');
 
-// Add 16 divs
-for (let i = 1; i < 257; i++) {
-  const div = document.createElement("div");
- 
- 
-  cdiv.appendChild(div);
+const cdiv = document.querySelector('.container');
+function createGrid(number){
+  
+  for (let i = 0; i < (number * number); i++) {
+    const div = document.createElement("div");
+   
+   
+    cdiv.appendChild(div);
+  }
+trail();
 }
+
+
+
+function trail() {
 const tiles = document.querySelectorAll('.container>div');
 tiles.forEach(tile => {
   tile.addEventListener('mouseenter', () => {
     // do something when tile is hovered over
     tile.style.backgroundColor = 'yellow';
 })});
+}
 
+
+
+const button = document.querySelector(".ask");
+button.addEventListener("click", () => {
+  const tiles = document.querySelectorAll('.container>div');
+  tiles.forEach(tile => {
+    tile.remove();
+  });
+
+let answer = prompt("number of grids? maximum 100");
+if(answer >=100){
+  alert("select a lesser number");
+  return;
+}
+else
+{
+
+  createGrid(answer);
+}
+
+});
+
+
+createGrid(16);
